6c2bd3fda67899cb32aba52ce76f11849d4bf18b
Vallabh Kansagara <vrkansagara@gmail.com>
2020-02-06 12:41:43 +0530

    Change into config path and updation of license url

--- src/PageCacheFactory.php
+++ src/PageCacheFactory.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @link      https://github.com/weierophinney/PhlySimplePage for the canonical source repository
+ * @link      https://github.com/phly/PhlySimplePage for the canonical source repository
  * @copyright Copyright (c) 2012-2020 Matthew Weier O'Phinney (https://mwop.net)
- * @license   https://github.com/weierophinney/PhlySimplePage/blog/master/LICENSE.md New BSD License
+ * @license   https://github.com/phly/PhlySimplePage/blob/master/LICENSE.md New BSD License
  */
 
-declare(strict_types=1);
+
 
 namespace PhlySimplePage;
 
